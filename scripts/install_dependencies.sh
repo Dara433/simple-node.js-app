ccf6c0991115ef60f4d098c3f07d5114d2c8f905
Dara Omotayo <omotayob1995@gmail.com>
2026-04-05 11:37:33 -0500

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Ensure ec2-user owns the deployment directory
+sudo chown -R ec2-user:ec2-user /home/ec2-user/simple-node-app
+sudo chmod -R 755 /home/ec2-user/simple-node-app
+
 cd /home/ec2-user/simple-node-app/src
 npm install
